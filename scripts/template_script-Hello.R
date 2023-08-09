0deab97ced50b954a3bc004c7d7b88cd868b2cfb
JarrettBlair <48661065+Jarrett-Blair@users.noreply.github.com>
2023-08-09 16:47:27 -0700

    Create template_script-Hello.R

---
+++ scripts/template_script-Hello.R
@@ -0,0 +1,18 @@
+#THIS IS A TEMPLATE SCRIPT - PLEASE SAVE YOURS UNDER A DIFFERENT NAME
+#BEFORE YOU COMMIT
+
+#Sage grouse data - (Your name's) code
+#Created: 9 Aug 2023 (EB)
+#Last edited: 
+
+
+
+#Libraries
+library(tidyverse)
+
+#Code
+
+#read in the data
+sage_grouse <- read.csv("data/sage_grouse_morphology.csv")
+
+print("Hello world")

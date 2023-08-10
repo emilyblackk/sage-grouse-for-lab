3d525274d279232c10d3f13985d87f429c91671a
kemarshall <kmarshall@zoology.ubc.ca>
2023-08-10 10:35:11 -0700

    Checking on the freeze tolerance of sage grouse

---
+++ scripts/template_script.kem.R
@@ -0,0 +1,17 @@
+#THIS IS A TEMPLATE SCRIPT - PLEASE SAVE YOURS UNDER A DIFFERENT NAME
+#BEFORE YOU COMMIT
+
+#Sage grouse data - (Your name's) code
+#Created: 9 Aug 2023 (EB)
+#Last edited: 10 Aug 2023 (EB)
+
+##what temperature do sage grouse freeze at?##
+
+#Libraries
+library(tidyverse)
+
+#Code
+
+#read in the data
+sage_grouse <- read.csv("data/sage_grouse_morphology.csv")
+

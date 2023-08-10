04fefc5ce575f818874262cbcdfe401d18d2518a
Emily Black <black.emily.nicole@gmail.com>
2023-08-10 10:37:17 -0700

    Added emily's script

---
+++ scripts/template_script_emily.R
@@ -0,0 +1,20 @@
+#THIS IS A TEMPLATE SCRIPT - PLEASE SAVE YOURS UNDER A DIFFERENT NAME
+#BEFORE YOU COMMIT
+
+#Sage grouse data - (Your name's) code
+#Created: 9 Aug 2023 (EB)
+#Last edited: 10 Aug 2023 (EB)
+
+
+
+#Libraries
+library(tidyverse)
+
+#Code
+
+#read in the data
+sage_grouse <- read.csv("data/sage_grouse_morphology.csv")
+
+#emily making a histogram of body weight
+hist(sage_grouse$weight_bird_kg)
+
